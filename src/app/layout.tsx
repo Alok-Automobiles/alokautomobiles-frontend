8cea930e606c15ea78452ebf6518b4e5f1e5f58c
RishiChaubey31 <cenationrishi31@gmail.com>
2025-11-05 12:34:26 +0530

    integrated new comp to contact section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LoadingScreen } from "@/components/Landing-page/loading-screen";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
         <div className="animate-fade-in">
           {children}
         </div>
+        <SpeedInsights />
       </body>
     </html>
   );
